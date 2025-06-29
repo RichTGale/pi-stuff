d5938101d5077d0f1033852dd58b8a0061c5325d
Richard Gale <richtgale@gmail.com>
2025-06-29 13:34:10 +1000

    Update my-autostart-apps.sh

--- my-autostart-apps.sh
+++ my-autostart-apps.sh
@@ -6,5 +6,5 @@ nmcli device connect wlan1
 sleep 30
 sudo systemctl restart openvpn-client@client.service
 
-#echo Press any button to end...
-#read pressEnter
+echo Press any button to end...
+read pressEnter
