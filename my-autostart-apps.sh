d1ad9aa80001a02c4eb3f4008d161b333e914f78
Richard Gale <richtgale@gmail.com>
2025-06-29 13:53:09 +1000

    Update my-autostart-apps.sh

--- my-autostart-apps.sh
+++ my-autostart-apps.sh
@@ -4,7 +4,10 @@ nmcli device disconnect wlan0
 sleep 30
 nmcli device connect wlan1
 sleep 30
+printf "Restarting VPN service. Network config will display once the VPN has restarted.\n"
 sudo systemctl restart openvpn-client@client.service
+sleep 30
+ip a
 
 echo Press any button to end...
 read pressEnter
