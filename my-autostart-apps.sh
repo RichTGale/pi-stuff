6589d82adffe5cefd1e10d12deea0ba75675434c
RichTGale <richtgale@gmail.com>
2025-07-29 14:23:43 +1000

    Removed VPN startup

--- my-autostart-apps.sh
+++ my-autostart-apps.sh
@@ -4,9 +4,9 @@ nmcli device disconnect wlan0
 sleep 30
 nmcli device connect wlan1
 sleep 30
-printf "Restarting VPN service. Network config will display once the VPN has restarted.\n"
-sudo systemctl restart openvpn-client@client.service
-sleep 30
+#printf "Restarting VPN service. Network config will display once the VPN has restarted.\n"
+#sudo systemctl restart openvpn-client@client.service
+#sleep 30
 ip a
 sudo motion
 $HOME/Programs/let-there-be-light
