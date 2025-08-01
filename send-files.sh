8338290f723a7c2f24d642828fe963bdcedb97ee
RichTGale <richtgale@gmail.com>
2025-08-01 10:17:29 +1000

    added sleep time

--- send-files.sh
+++ send-files.sh
@@ -8,10 +8,12 @@ while [ 1 ]
 do
     if [ ! -z "$(ls -A $host )" ]; # Check that there are videos
     then
+        sleep 70
         videos=($host/*)   # Get the array of detections
         v=0
         for v in "${!videos[@]}"; do
             scp ${videos[v]} $remote
+            sleep 70
         done
         sudo rm -f $host/*
     fi
