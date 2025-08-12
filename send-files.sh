23aea4fb73a4d275ee4fa57e632f940d0ca43399
Richard Gale <richtgale@gmail.com>
2025-08-12 20:46:18 +1000

    Update send-files.sh

--- send-files.sh
+++ send-files.sh
@@ -9,14 +9,17 @@ while [ 1 ]
 do
     if [ ! -z "$(ls -A $host )" ]; # Check that there are videos
     then
+        printf "A video was recorded...\n"
         sleep 70
         videos=($host/*)   # Get the array of detections
         v=0
         for v in "${!videos[@]}"; do
+            printf "Sending video to client(s)...\n"
             scp ${videos[v]} $remote2
             scp ${videos[v]} $remote1
             sleep 70
         done
+        printf "Deleting local video(s)...\n"
         sudo rm -f $host/*
     fi
 done
