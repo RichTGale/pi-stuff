8bedfdbb59733d2b72db05874128cb897b2f323c
Richard Gale <richtgale@gmail.com>
2025-08-19 14:39:43 +1000

    Update my-autostart-apps.sh

--- my-autostart-apps.sh
+++ my-autostart-apps.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 nmcli device disconnect wlan0
-sleep 30
+sleep 15
 nmcli device connect wlan1
-sleep 30
+sleep 15
 printf "Restarting VPN service. Network config will display once the VPN has restarted.\n"
 sudo systemctl restart openvpn-client@client.service
 sleep 30
 ip a
 lxterminal --geometry=300x300 -e "sudo $HOME/Programs/let-there-be-light"
-sleep 15
+sleep 10
 lxterminal --geometry=300x300 -e "$HOME/Programs/send-files.sh"
-sleep 15
+sleep 10
 sudo motion
 
 
