64ceef04f360516ae59d2b5e73b8d2dc55840ab2
Richard Gale <richtgale@gmail.com>
2025-08-19 14:39:04 +1000

    Update my-autostart-apps.sh

--- my-autostart-apps.sh
+++ my-autostart-apps.sh
@@ -8,9 +8,12 @@ printf "Restarting VPN service. Network config will display once the VPN has res
 sudo systemctl restart openvpn-client@client.service
 sleep 30
 ip a
-lxterminal --geometry=300x300 -e "$HOME/Programs/send-files.sh"
-sudo motion
 lxterminal --geometry=300x300 -e "sudo $HOME/Programs/let-there-be-light"
+sleep 15
+lxterminal --geometry=300x300 -e "$HOME/Programs/send-files.sh"
+sleep 15
+sudo motion
+
 
 echo Press any button to end...
 read pressEnter
