88bc663b9ade51f2f827d434e6dc64c6c62133f9
RichTGale <richtgale@gmail.com>
2025-09-10 00:11:28 +1000

    Tried to fix some things

--- rpi02w/configure-pi02w-00.sh
+++ rpi02w/configure-pi02w-00.sh
@@ -1,14 +1,15 @@
 #1/bin/bash
 
+repo_name="pi-stuff/rpi02w"
 
-printf "\nCreating $HOME/Programming and $HOME/Programs directories to install stuff in later...\n"
-if [ ! -d $HOME/Programming ]; then
-    mkdir $HOME/Programming
+printf "\nCreating ${HOME}/Programming and ${HOME}/Programs directories to install stuff in later...\n"
+if [ ! -d ${HOME}/Programming ]; then
+    mkdir ${HOME}/Programming
 fi
 
-if [ ! -d $HOME/Programs ]; then
-    mkdir $HOME/Programs
-	mkdir $HOME/Programs/videos
+if [ ! -d ${HOME}/Programs ]; then
+    mkdir ${HOME}/Programs
+	mkdir ${HOME}/Programs/videos
 fi
 
 printf "\nDoing a full system update and upgrade...\n"
@@ -21,24 +22,24 @@ sudo apt install vim git zsh dkms build-essential cmake unzip openvpn libelf-dev
 printf "\nWould you like to configure vim? (Y/n):"
 read install_vim
 printf "\n"
-if [ "$install_vim" != "n" ]; then
+if [ "${install_vim}" != "n" ]; then
 	printf "\nConfiguring vim...\n"
-	cd $HOME/Programming
+	cd ${HOME}/Programming
 	curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	cp $HOME/Programming/pi-stuff/.vimrc $HOME/
+	cp ${HOME}/Programming/${repo_name}/.vimrc ${HOME}/
 	vim -c PlugInstall
 fi
 
 printf "\nWould you like to configure zsh? (Y/n):"
 read install_zsh
 printf "\n"
-if [ "$install_zsh" != "n" ]; then
+if [ "${install_zsh}" != "n" ]; then
 	printf "\nConfiguring zsh...\n"
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-	cd $HOME/Programming
-	cp $HOME/Programming/pi-stuff/.zshrc $HOME/
+	cd ${HOME}/Programming
+	cp ${HOME}/Programming/${repo_name}/.zshrc ${HOME}/
 	printf "\nSwitching default shell to zsh...\n"
 	chsh -s $(which zsh)
 fi
@@ -47,14 +48,10 @@ printf "\nCleaning up...\n"
 sudo apt autoremove -y
 
 printf "\nCopying autostart files...\n"
-cp $HOME/Programming/pi-stuff/configure-pi02w-01.sh $HOME/Programming/pi-stuff/configure-pi02w-02.sh $HOME/Programming/pi-stuff/configure-pi02w-03.sh $HOME/Programming/pi-stuff/configure-pi02w-04.sh $HOME/Programming/pi-stuff/configure-pi02w-05.sh $HOME/Programming/pi-stuff/my-autostart-apps.sh -t $HOME/Programs
-chmod +x $HOME/Programs/configure-pi02w-01.sh
-chmod +x $HOME/Programs/configure-pi02w-02.sh
-chmod +x $HOME/Programs/configure-pi02w-03.sh
-chmod +x $HOME/Programs/configure-pi02w-04.sh
-chmod +x $HOME/Programs/configure-pi02w-05.sh
-chmod +x $HOME/Programs/my-autostart-apps.sh
-sudo cp $HOME/Programming/pi-stuff/configure-pi02w-01.desktop /etc/xdg/autostart/
+cp -t ${HOME}/Programs ${HOME}/Programming/${repo_name}/configure*.sh ${HOME}/Programming/${repo_name}/my-autostart-apps.sh
+chmod +x ${HOME}/Programs/configure*.sh
+chmod +x ${HOME}/Programs/my-autostart-apps.sh
+sudo cp ${HOME}/Programming/${repo_name}/configure-pi02w-01.desktop /etc/xdg/autostart/
 
 sudo raspi-config
 
--- rpi02w/configure-pi02w-01.sh
+++ rpi02w/configure-pi02w-01.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+repo_name="pi-stuff/rpi02w"
+
 printf "\nWould you like to change the amount of space allocated to the swapfile? (Y/n): "
 read change_swap
 printf "\n"
-if [ "$change_swap" != "n" ]; then
+if [ "${change_swap}" != "n" ]; then
     printf "\nOpening the swap config file for editing...\n"
     sudo dphys-swapfile swapoff
     sudo vim /etc/dphys-swapfile
@@ -12,6 +14,6 @@ if [ "$change_swap" != "n" ]; then
 fi
 
 sudo rm /etc/xdg/autostart/configure-pi02w-01.desktop 
-sudo cp $HOME/Programming/pi-stuff/configure-pi02w-02.desktop /etc/xdg/autostart/
+sudo cp ${HOME}/Programming/${repo_name}/configure-pi02w-02.desktop /etc/xdg/autostart/
 
 sudo reboot
