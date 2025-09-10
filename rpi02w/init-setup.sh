0a70b1ed2e79792d0104077c82b122435dd9856f
RichTGale <richtgale@gmail.com>
2025-09-10 13:44:39 +1000

    Fixed some bugs

--- rpi02w/init-setup.sh
+++ rpi02w/init-setup.sh
@@ -1,5 +1,8 @@
 #1/bin/bash
 
+device_path="|${device_path}|"
+device_path=s|${device_path}|
+s device_path=|${device_path}|
 #repo_name=${PWD##*/}
 #repo_name=${repo_name:-/}
 device_path=${PWD}
@@ -74,16 +77,49 @@ then
     sudo dphys-swapfile swapon
 fi
 
-# Giving the other files the device_path
-sed -i "3i ${device_path}" ${device_path}/install-zerosec.sh
-sed -i "3i ${device_path}" ${device_path}/finalise-setup.sh
+printf "Installing zerosec...\n"
+
+printf "\nInstalling motion...\n"
+sudo apt install motion -y
+sudo cp ${device_path}/motion.conf /etc/motion/
+
+printf "\nInstalling pigpio...\n"
+cd ${HOME}/Programs
+wget https://github.com/joan2937/pigpio/archive/master.zip
+unzip master.zip
+rm -rf master.zip
+cd pigpio-master
+make
+sudo make install
+
+printf "\nInstalling TP-Link Archer T2U Plus [RTL8821AU] driver...\n"
+sudo apt install raspberrypi-kernel-headers build-essential git -y
+cd ${HOME}/Programs
+git clone https://github.com/lwfinger/rtw88
+cd rtw88
+sudo dkms install $PWD
+sudo make install_fw
+
+printf "\nBuilding IR light program...\n"
+cp ${device_path}/let-there-be-light.c ${HOME}/Programs
+cd ${HOME}/Programs
+gcc -Wall -pthread -o let-there-be-light let-there-be-light.c -lpigpio -lrt
+
+printf "\nCopying remote sending file(s)...\n"
+cp ${device_path}/send-files.sh ${HOME}/Programs/
+chmod +x $HOME/Programs/send-files.sh
 
 printf "\nCopying autostart files...\n"
-sudo cp -t /etc/xdg/autostart ${device_path}/install-zerosec.desktop
-cp -t ${HOME}/Programs ${device_path}/install-zerosec.sh 
-cp -t ${HOME}/Programs ${device_path}/finalise-setup.sh 
+sudo cp -t /etc/xdg/autostart ${device_path}/autoapps.sh
 cp -t ${HOME}/Programs ${device_path}/autoapps.sh
 chmod +x ${HOME}/Programs/*.sh
+
+printf "\nCopying config files...\n"
+sudo cp ${device_path}/config.txt /boot/firmware/
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove -y
 
 sudo raspi-config
 
