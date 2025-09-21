618fd403fa4f0095d3991fc1b7b48be1ef1c4b90
Richard Gale <35957595+RichTGale@users.noreply.github.com>
2025-09-21 20:55:42 +1000

    Update autoapps.sh

--- rpi02w/autoapps.sh
+++ rpi02w/autoapps.sh
@@ -2,26 +2,28 @@
 
 #nmcli device disconnect wlan0
 #sleep 5
-nmcli device connect wlan1
-sleep 5
+#nmcli device connect wlan1
+#sleep 5
 
-printf "Restarting VPN service. Network config will display once the VPN has restarted.\n"
-sudo systemctl restart openvpn-client@client.service
-sleep 5
+#printf "Restarting VPN service. Network config will display once the VPN has restarted.\n"
+#sudo systemctl restart openvpn-client@client.service
+#sleep 5
 
-lxterminal -e "sudo ${HOME}/Programs/reconnect.sh"
-sleep 5
+#lxterminal -e "s${HOME}/Programs/reconnect.sh"
+#sleep 5
 
-lxterminal -e "sudo ${HOME}/Programs/send-files.sh"
-sleep 5
+#lxterminal -e "${HOME}/Programs/send-files.sh"
+#sleep 5
 
-lxterminal -e "sudo ${HOME}/Programs/irreflect -t 18"
-sleep 5
+#lxterminal -e "sudo ${HOME}/Programs/irreflect -t 18"
+#sleep 5
 
-sudo motion
+#sudo motion
 
-ip a
-sleep 2
+#ip a
+#sleep 2
+
+printf "Uncomment lines in this script.\n"
 
 echo Press any button to end...
 read pressEnter
