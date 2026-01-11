756e5ce72a1dbd460cdd4afc9f6637474d196f01
Richard Gale <35957595+RichTGale@users.noreply.github.com>
2026-01-11 15:36:20 +1100

    Update macdump.sh

--- rpi5/macdump.sh
+++ rpi5/macdump.sh
@@ -7,10 +7,6 @@ printf "Would you like to install aircrack-ng? This will update your system and 
 read install_aircrack
 printf "\n"
 
-printf "Are you using wlan1 (tp link ac600 wifi aerial)? <y/N>: "
-read using_wlan1
-printf "\n"
-
 if [ "${install_aircrack}" == "y" ];
 then
   sudo apt update ; sudo apt upgrade ; sudo apt install aircrack-ng macchanger
@@ -18,20 +14,15 @@ fi
 
 sudo airmon-ng check kill
 
-if [ "${using_wlan1}" == "y" ];
-then
-  sudo ip link set wlan0 down
-  sleep 2
-  sudo ip link set wlan1 up
-  sudo airmon-ng start wlan1
-  sleep 2
-  sudo ip link set wlan1mon down
-  sudo macchanger -r wlan1mon
-  sudo ip link set wlan1mon up
-  sudo airodump-ng --write ${HOME}/Documents/dump --output-format csv wlan1mon
-else
-  printf "Monitor mode not supported on this wireless interface\n"
-fi
+sudo ip link set wlan0 down
+sleep 2
+sudo ip link set wlan1 up
+sudo airmon-ng start wlan1
+sleep 2
+sudo ip link set wlan1mon down
+sudo macchanger -r wlan1mon
+sudo ip link set wlan1mon up
+sudo airodump-ng --write ${HOME}/Documents/dump --output-format csv wlan1mon
 
-read pressEnter
+sudo airmon-ng stop wlan1mon
 
